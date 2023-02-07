cd5b5869241aba591d97fe8a3372929850f6a2e6
Sinan Altaii <sinan.altaii@limetta.se>
2023-02-07 22:34:12 +0100

    Updated search helper by cahing the result 1 min and comments as reference for multisearch

--- Helpers/SearchHelper.cs
+++ Helpers/SearchHelper.cs
@@ -1,10 +1,12 @@
 ﻿using Alloy12.Models.Pages;
 using EPiServer.Find;
+using EPiServer.Find.Api.Facets;
 using EPiServer.Find.Cms;
 using EPiServer.Find.Framework;
 using EPiServer.Find.Framework.Statistics;
 using EPiServer.Find.Statistics;
 using EPiServer.Find.UnifiedSearch;
+using PageData = EPiServer.Core.PageData;
 
 namespace Alloy12.Helpers
 {
@@ -18,7 +20,7 @@ namespace Alloy12.Helpers
                 .UnifiedSearch()
                 .For(query, x => x.Analyzer = Language.English.Analyzer)
                 .Track()
-                .StatisticsTrack();
+                .StaticallyCacheFor(TimeSpan.FromMinutes(1));
             var searchResult = unifiedSearch.GetResult();
             
             return searchResult;
@@ -27,9 +29,14 @@ namespace Alloy12.Helpers
         public IContentResult<IContent> SearchForContent(string query)
         {
             // Search<IContent> could be any page type
+            // For multiple search https://docs.developers.optimizely.com/digital-experience-platform/v1.1.0-search-and-navigation/docs/multiple-searches-in-one-request
             var searchResult = SearchClient.Instance
                 .Search<SitePageData>()
                 .For(query, x => x.Analyzer = Language.English.Analyzer)
+                //.InField(f => f.MainBody)
+                //.AndInField(f => f.Name)
+                //.AndInField(f => f.PageName)
+                //.AndInField(f => f.TeaserText)
                 .FilterForVisitor() // Group of filters including ExcludeDelete, PublishedInCurrentLanguage, and FilterOnReadAccess https://docs.developers.optimizely.com/digital-experience-platform/v1.1.0-search-and-navigation/docs/filters
                 .Track()
                 .StatisticsTrack()
